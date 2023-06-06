c79dd6d730a5639d9cb19e0108d3edbb7fae1b6f
Andrew Smith <47258805+smithandrewk@users.noreply.github.com>
2023-06-06 11:38:33 -0400

    Create KeepScreenOn.kt

---
+++ KeepScreenOn.kt
@@ -0,0 +1,10 @@
+import android.view.WindowManager
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContent {
+        }
+  }
